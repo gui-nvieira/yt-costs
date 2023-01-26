64108e66b3f92e7bb046ce2f474293f293e6b272
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2023-01-26 20:11:35 -0300

    Done Confirmation message on project page

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -4,10 +4,26 @@ import { useState, useEffect } from "react"
 function Message({ type, msg }) {
     const [visible, setVisible] = useState(false)
 
-    return (
-        <div className={`${styles.message} ${styles[type]}`}>
-            {msg}
-        </div>
+    useEffect(() => {
+        if (!msg) {
+            setVisible(false)
+            return
+        }
+        setVisible(true)
+        const timer = setTimeout(() => {
+            setVisible(false)
+        }, 3000)
+        return () => clearTimeout(timer)
+    }, [msg])
+
+    return (<>
+        {visible && (
+            <div className={`${styles.message} ${styles[type]}`}>
+                {msg}
+            </div>
+        )}
+    </>
+
     )
 }
 
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,11 +1,19 @@
 import Message from "../layout/Message"
-
+import { useLocation } from "react-router-dom"
 
 function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
+
     return (
         <div>
             <h1>Meus Projetos</h1>
-            <Message msg="Alguma Mensagem" type="error" />
+            {message && <Message msg={message} type="sucess" />}
         </div>
     )
 }
