a26ee0a65de1826b82c16b421244f0af1366340f
Guilherme Vieira <81487846+gui-nvieira@users.noreply.github.com>
2023-01-31 09:03:59 -0300

    Edit Projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import Message from "../layout/Message"
 import { useLocation } from "react-router-dom"
+import styles from "./Projects.module.css"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 function Projects() {
 
@@ -11,9 +14,16 @@ function Projects() {
 
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles['project_container']}>
+            <div className={styles['title_container']}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"></LinkButton>
+            </div>
+
             {message && <Message msg={message} type="sucess" />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
